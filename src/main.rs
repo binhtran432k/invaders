2974cc0dcb561215b3c99b3c0f56c4cd7d919549
Binh Duc Tran <binhtran432k@gmail.com>
2024-02-10 22:51:29 +0700

    feat(player): add player render and control

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,8 @@ use crossterm::{
     ExecutableCommand,
 };
 use invaders::{
-    frame::{self, new_frame, Frame},
+    frame::{self, new_frame, Frame, Drawable},
+    player::Player,
     render,
 };
 use rusty_audio::Audio;
@@ -49,14 +50,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     });
 
     // Game loop
+    let mut player = Player::new();
     'gameloop: loop {
         // Per-frame init
-        let curr_frame = new_frame();
+        let mut curr_frame = new_frame();
 
         // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
                 match key_event.code {
+                    KeyCode::Left => player.move_left(),
+                    KeyCode::Right => player.move_right(),
                     KeyCode::Esc | KeyCode::Char('q') => {
                         audio.play("lose");
                         break 'gameloop;
@@ -67,6 +71,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
 
         // Draw & render
+        player.draw(&mut curr_frame);
         let _ = render_tx.send(curr_frame);
         thread::sleep(Duration::from_millis(1));
     }
