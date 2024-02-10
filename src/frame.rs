52f2e0859b15e4a46fa8857f1b0428ae741484b6
Binh Duc Tran <binhtran432k@gmail.com>
2024-02-10 21:49:59 +0700

    refactor(frame): use functional style

--- src/frame.rs
+++ src/frame.rs
@@ -3,19 +3,11 @@ use crate::{NUM_COLS, NUM_ROWS};
 pub type Frame = Vec<Vec<&'static str>>;
 
 pub fn new_frame() -> Frame {
-    let mut cols = Vec::with_capacity(NUM_COLS);
-    for _ in 0..NUM_COLS {
-        let mut col = Vec::with_capacity(NUM_ROWS);
-        for _ in 0..NUM_ROWS {
-            col.push(" ")
-        }
-        cols.push(col)
-    }
-    cols
+    (0..NUM_COLS)
+        .map(|_| (0..NUM_ROWS).map(|_| " ").collect())
+        .collect()
 }
 
 pub trait Drawable {
-    fn draw(&self, frame: &mut Frame) {
-        unimplemented!()
-    }
+    fn draw(&self, frame: &mut Frame) {}
 }
